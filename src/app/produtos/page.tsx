271d836e2e0618820a3bf5a0079f43a62c0f6244
Asteriuz <gutocebola@gmail.com>
2023-10-20 20:13:24 -0300

    Updated Produtos

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -5,7 +5,7 @@ export default function Produtos() {
     <div className="flex flex-col items-center gap-y-8 mt-8">
       <h1 className="text-4xl">PRODUTOS</h1>
       <ul className="flex gap-x-20 text-2xl text-white flex-wrap gap-y-10 items-center justify-center">
-        <div className="px-2 bg-laranja-claro hover:bg-white hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
+        <div className="px-2 bg-laranja-claro hover:bg-transparent hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
           <li>
             <Link
               className="hover:text-laranja-claro  duration-300 transition-colors"
@@ -15,7 +15,7 @@ export default function Produtos() {
             </Link>
           </li>
         </div>
-        <div className="px-2 bg-laranja-claro hover:bg-white hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
+        <div className="px-2 bg-laranja-claro hover:bg-transparent hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
           <li>
             <Link
               className="hover:text-laranja-claro  duration-300 transition-colors"
@@ -25,7 +25,7 @@ export default function Produtos() {
             </Link>
           </li>
         </div>
-        <div className="px-2 bg-laranja-claro hover:bg-white hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
+        <div className="px-2 bg-laranja-claro hover:bg-transparent hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
           <li>
             <Link
               className="hover:text-laranja-claro  duration-300 transition-colors"
@@ -35,7 +35,7 @@ export default function Produtos() {
             </Link>
           </li>
         </div>
-        <div className="px-2 bg-laranja-claro hover:bg-white hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
+        <div className="px-2 bg-laranja-claro hover:bg-transparent hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
           <li>
             <Link
               className="hover:text-laranja-claro  duration-300 transition-colors"
@@ -45,7 +45,7 @@ export default function Produtos() {
             </Link>
           </li>
         </div>
-        <div className="px-2 bg-laranja-claro hover:bg-white hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
+        <div className="px-2 bg-laranja-claro hover:bg-transparent hover:border-laranja-claro hover:border-2 hover:scale-105 transition-all duration-500 ease-in-out py-1 rounded-lg ">
           <li>
             <Link
               className="hover:text-laranja-claro  duration-300 transition-colors"
